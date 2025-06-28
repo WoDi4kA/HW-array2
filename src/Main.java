4ab640bc348f0734fb486b1e03770e2473512a79
wodi4ka <platonadanaya@gmail.com>
2025-06-28 17:59:50 +0300

    Add all answers

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,58 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        //Task#1
+        int [] array = {5000, 3500, 4200, 3700, 2900};
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            count += array[i];
+        }
+        System.out.println("Сумма трат за меся составила " + count + " рублей");
+
+        System.out.println();
+        //Task#2
+
+        int minimalValue = 99999;
+        int maximumValue = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maximumValue) {
+                maximumValue = array[i];
+            }
+            if (array[i] < minimalValue) {
+                minimalValue = array[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " + maximumValue + " рублей");
+        System.out.println("Минимальная сумма трат за неделю составила " + minimalValue + " рублей");
+
+        System.out.println();
+        //Task#3
+
+        float count1 = 0;
+        double result = 0;
+        for (int i = 0; i < array.length; i++) {
+            count1 += array[i];
+        }
+        result = count1 / 5;
+        System.out.println(count1 / 5);
+
+        System.out.println();
+        //Task#4
+
+        char [] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i > 0; i--) {
+            for (int j = 0; j < reverseFullName.length - 1; j++) {
+                char temp = reverseFullName[j];
+                reverseFullName[j] = reverseFullName[i];
+                reverseFullName[i] = temp;
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(reverseFullName));
 
     }
 }
